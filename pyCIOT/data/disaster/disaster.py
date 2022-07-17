338b3e44bd0b677bda5a7c371a957bd8c16f679f
Wlogsky <wlogsky666@gmail.com>
2022-07-18 06:15:20 +0800

    Add disastor module

--- pyCIOT/data/disaster/disaster.py
+++ pyCIOT/data/disaster/disaster.py
@@ -21,9 +21,8 @@ class Disastor(Module):
         if typ not in alert_types:
             raise Exception("Unknown type is provided:", typ)
 
-        pass
-        # AlertType=typ
-        # return json.loads(requests.get())
+        url = f"https://alerts.ncdr.nat.gov.tw/JSONAtomFeed.ashx?AlertType={typ}"
+        return json.loads(requests.get(url).content)
 
     def get_notice(self, typ: str):
         """
@@ -32,5 +31,23 @@ class Disastor(Module):
         if typ not in notice_types:
             raise Exception("Unknown type is provided:", typ)
 
-        pass
-        # return json.loads(requests.get(url, verify=self.verify).content)
+        if typ == "CEOCopen":
+            url = "https://portal2.emic.gov.tw/Pub/EEM2/OpenData/CEOCopen.json"
+        elif typ == "Disaster":
+            # CSV ??
+            url = "https://portal2.emic.gov.tw/Pub/DIM2/OpenData/Disaster.csv"
+            raise Exception("Data source is not JSON file.")
+        elif typ == "Shelter":
+            # XML ????
+            url = "https://portal2.emic.gov.tw/Pub/EEA2/OpenData/Shelter.xml"
+            raise Exception("Data source is not JSON file.")
+        elif typ == "EMIC_Resource":
+            # ZIP ????????????????
+            url = "https://portal2.emic.gov.tw/Pub/EDD2/OpenData/EMIC_Resource.zip"
+            raise Exception("Data source is not JSON file.")
+        elif typ == "EvaculateArea":
+            url = "https://portal2.emic.gov.tw/Pub/EEA2/OpenData/EvaculateArea.json"
+        else:
+            url = f"https://portal2.emic.gov.tw/Pub/ERA2/OpenData/{typ}.json"
+
+        return json.loads(requests.get(url).content)
