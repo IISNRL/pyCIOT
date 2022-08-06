196ecb554f052784c1721e4b64c6b3717acd0ac7
Wlogsky <wlogsky666@gmail.com>
2022-08-07 04:47:37 +0800

    Prototypes of features

--- pyCIOT/data/air/air.py
+++ pyCIOT/data/air/air.py
@@ -1,7 +1,7 @@
 from ..module import Module
 from ..utils.crawler import Crawler
 from ..utils.url import UrlBuilder, Select, OrderBy, Pagination, Filter, Expand, Expands
-from ..utils.op import EQ, LE
+from ..utils.op import EQ, GE, LE, GEODISTANCE
 
 from typing import Any
 
@@ -21,9 +21,7 @@ class Air(Module):
                 f"{typ}:{name}" for typ in self._sources for name in self._sources[typ]
             ]
 
-    def get_data(
-        self, src: str, stationID: str = None, timestamp: str = None
-    ) -> "list[Any]":
+    def get_data(self, src: str, **kwargs) -> "list[Any]":
         """
         Get sensing data with optional stationID and timestamp.
 
@@ -31,10 +29,22 @@ class Air(Module):
         ----------
         src:
             Project src from `get_source`
-        stationID:
-            Specifies the station
-        timestamp:
-            Specifies the timestamp
+
+        (Optional)
+        location: {
+            latitude: 23.xxx,
+            longitude: 121.xxx,
+            distance: 3,  (km)
+        }
+
+        (Optional)
+        stationIds: list[]
+
+        (Optional)
+        time_range: {
+            start: timestamp1,
+            end: timestamp2
+        }
 
         Returns
         ---------
@@ -57,24 +67,60 @@ class Air(Module):
                 ),
             ]
         )
-        if timestamp:
-            # TODO: Check timestamp format
+
+        if "time_range" in kwargs:
+            time_range = kwargs["time_range"]
+            start, end = time_range.get("start"), time_range.get("end")
+
+            if not (start and end):
+                raise Exception("Invalid time_range")
+
             expands.get_expand("Datastreams/Observations").set_filter(
-                Filter([LE("phenomenonTime", timestamp)])
+                Filter(
+                    [
+                        GE("phenomenonTime", start),
+                        LE("phenomenonTime", end),
+                    ]
+                )
             )
 
-        filter = self.filter_parser(source["filters"])
-        if stationID:
-            filter.set_filter(EQ("properties/stationID", stationID))
+        _filter = self.filter_parser(source["filters"])
+        if "stationIds" in kwargs:
+            stationIds = kwargs["stationIds"]
+            if len(stationIds) == 1:
+                # TODO: Survey if there's a way to pass several Ids into query
+                _filter.set_filter(EQ("properties/stationID", stationIds[0]))
+
+        if "location" in kwargs:
+            location = kwargs["location"]
+            latitude, longitude, distance = (
+                location["latitude"],
+                location["longitude"],
+                location["distance"],
+            )
+            if latitude is None or longitude is None or distance is None:
+                raise Exception("Invalid location")
+
+            _filter.set_filter(GEODISTANCE(latitude, longitude, distance))
 
         url = UrlBuilder(
             source["base_url"],
             expands=expands,
-            filter=filter,
+            filter=_filter,
             pagination=Pagination(1, 1001),
         )
         res = Crawler().get(url.get_thing())
-        return self.parse_data(res)
+
+        if "stationIds" in kwargs and len(kwargs["stationIds"]) > 1:
+            stationIds = kwargs["stationIds"]
+            return list(
+                filter(
+                    lambda x: x["properties"]["stationID"] in stationIds,
+                    self.parse_data(res),
+                )
+            )
+        else:
+            return self.parse_data(res)
 
     def get_station(self, src: str, stationID: str = None) -> "list[Any]":
         """
