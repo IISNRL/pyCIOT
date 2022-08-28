3244bb357859cc88ee681ff6e9cdbe94acce6fb9
Wlogsky <wlogsky666@gmail.com>
2022-08-28 23:07:10 +0800

    Remove StationID arr filtering

--- pyCIOT/data/air/air.py
+++ pyCIOT/data/air/air.py
@@ -22,34 +22,6 @@ class Air(Module):
             ]
 
     def get_data(self, src: str, **kwargs) -> "list[Any]":
-        """
-        Get sensing data with optional stationID and timestamp.
-
-        Parameters
-        ----------
-        src:
-            Project src from `get_source`
-
-        (Optional)
-        location: {
-            latitude: 23.xxx,
-            longitude: 121.xxx,
-            distance: 3,  (km)
-        }
-
-        (Optional)
-        stationIds: list[]
-
-        (Optional)
-        time_range: {
-            start: timestamp1,
-            end: timestamp2
-        }
-
-        Returns
-        ---------
-
-        """
         try:
             typ, org = src.split(":")
             source = self._sources[typ][org]
@@ -85,12 +57,9 @@ class Air(Module):
                 )
             ).set_pagination(Pagination(end=1 + num_of_data))
 
-        _filter = self.filter_parser(source["filters"])
-        if "stationIds" in kwargs:
-            stationIds = kwargs["stationIds"]
-            if len(stationIds) == 1:
-                # TODO: Survey if there's a way to pass several Ids into query
-                _filter.set_filter(EQ("properties/stationID", stationIds[0]))
+        filter = self.filter_parser(source["filters"])
+        if "stationID" in kwargs:
+            filter.set_filter(EQ("properties/stationID", kwargs["stationID"]))
 
         if "location" in kwargs:
             location = kwargs["location"]
@@ -102,42 +71,18 @@ class Air(Module):
             if latitude is None or longitude is None or distance is None:
                 raise Exception("Invalid location")
 
-            _filter.set_filter(GEODISTANCE(latitude, longitude, distance))
+            filter.set_filter(GEODISTANCE(latitude, longitude, distance))
 
         url = UrlBuilder(
             source["base_url"],
             expands=expands,
-            filter=_filter,
+            filter=filter,
             pagination=Pagination(1, 1001),
         )
         res = Crawler().get(url.get_thing())
-
-        if "stationIds" in kwargs and len(kwargs["stationIds"]) > 1:
-            stationIds = kwargs["stationIds"]
-            return list(
-                filter(
-                    lambda x: x["properties"]["stationID"] in stationIds,
-                    self.parse_data(res),
-                )
-            )
-        else:
-            return self.parse_data(res)
+        return self.parse_data(res)
 
     def get_station(self, src: str, stationID: str = None) -> "list[Any]":
-        """
-        Get locations of sensing devices
-
-        Parameters
-        ----------
-        src:
-            Project src from `get_source`
-        stationID:
-            Specifies the station
-
-        Returns
-        ----------
-
-        """
         try:
             typ, org = src.split(":")
             source = self._sources[typ][org]
